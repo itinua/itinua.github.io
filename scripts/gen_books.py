b091a527dc0b3e7ed21fb9aea01bc35c109ac6b4
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2015-07-23 18:41:30 +0300

    add PDFs fix paths

---
+++ scripts/gen_books.py
@@ -0,0 +1,19 @@
+import os
+import re
+os.system("rm -rf ../print/pdf")
+os.system("mkdir ../print/pdf")
+for fn in os.listdir('../_posts'):
+    if "[" in fn:
+        for md in os.listdir('../_posts/'+fn):
+            if ".md" in md:
+                path = '../_posts/'+fn+"/"+md
+                name = re.sub('[0-9]*-[0-9]*-[0-9]*-','',md)
+                name = name.replace(".md",".pdf")
+                path = path.replace("[","\[").replace("]","\]").replace(" ","\ ")
+                print path
+                os.system("pandoc --template=my.latex --variable sansfont=Arial --variable fontsize=12pt -V geometry:margin=1in -V mainfont=Georgia  -s "+path+" --latex-engine=lualatex  -o ../print/pdf/"+name)
+
+
+
+
+
---
+++ scripts/test.sh
@@ -0,0 +1,1 @@
+pandoc --template=my.latex --variable sansfont=Arial --variable fontsize=12pt -V geometry:margin=1in -V mainfont=Georgia  -s kak.md --latex-engine=lualatex  -o demo.pdf
