f4ec73c8cbc443fcc4d555ebc307a3ac559b3025
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2015-08-10 18:20:04 +0300

    updates

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#pandoc --template=my.latex --variable sansfont=Arial --variable fontsize=12pt -V geometry:margin=1in -V mainfont=Georgia -s kak.md --latex-engine=xelatex  -o demo.pdf
-pandoc --template=default.html -s kak.md -o demo.html
+pandoc --template=my.latex --variable sansfont=Arial --variable fontsize=12pt -V geometry:margin=1in -V mainfont=Georgia -s kak.md --latex-engine=xelatex  -o demo.pdf
+#pandoc --template=default.html -s kak.md -o demo.html
