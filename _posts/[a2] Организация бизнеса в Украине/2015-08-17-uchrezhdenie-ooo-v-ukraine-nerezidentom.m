f2cee201d4b6938bc107f86d21ef8fb9054dfd71
annaivanenko <anna1.ivanenko@gmail.com>
2015-08-17 15:16:16 +0300

    Create 2015-08-17-uchrezhdenie-ooo-v-ukraine-nerezidentom.m

---
+++ _posts/[a2] Организация бизнеса в Украине/2015-08-17-uchrezhdenie-ooo-v-ukraine-nerezidentom.m
@@ -0,0 +1,58 @@
+---
+layout: article
+categories: [a2, actual, featured]
+title: Set up a company by non-resident (Учреждение ООО в Украине нерезидентом)
+img: un.jpg
+author: katerina-timchenko
+---
+Bellow you can find the list of documents to set up a company by non-resident. Please, note that points 1-3 referrer to 
+foreign legal entities founding the company in Ukraine. If the founder is a foreigner individual, instead of that documents
+he/she would need to provide duly legalized (or appostilled or just notarized, depending of the origin country) Power or 
+Attorney or to be presented personally in Ukraine to sign statutory and other documents of the company at notary.
+
+**1. A decision of company-founder to establish a limited liability company (LLC) in Ukraine**
+
+This document may be in the form of the Minutes of Shareholders/Participants/ of the company or other authorized body of the
+company. In this document the company-founder shall define the type (LLC) and the name of the company to be established in 
+Ukraine, to appoint representative to be presented on General Meeting of Participants of the Ukrainian LLC and sign the 
+statutory documents and Minutes of the Ukrainian LLC from behalf of the company.
+
+**2. An extract from commercial/court/banking registry issued in the country of the founder confirming the state registration 
+of the company-founder**
+
+Please, be informed that starting from 2010 the extract shall contain the beneficial owners - individuals. It means that in 
+case the obtained extract contains information of the founders which are legal entities, we would need the extracts of these
+entities and so on till the individuals will be shown.
+
+Apart from the above, for the purpose of the company registration, we might need the copies of founding documents 
+(Charter, Articles of incorporation etc) of the companies-founders. Though it is not requirement of the law, but some of the
+authorities might require it).
+
+It should be mentioned though that there is a legal way to avoid this requirement, which would make the process of 
+registration of company a bit longer.
+
+**3. Power of attorney authorizing persons who sign the foundation documents (e.g. Charter) and the Minutes of the General **
+**Meeting of Participants/Founders on behalf of the companies-founders and individuals who will take all actions required for**
+**the registration of LLC in Ukraine**
+
+The power of attorney is required if none of the individuals indicated in the extract (see item 2 above) as those who has 
+authority to represent the founder is going to be physically present in Ukraine to sign the Articles, minutes and other 
+documents (e.g. a lease agreement) of Ukrainian LLC in front of the notary public.
+
+**4. Lease agreement**
+
+The company shall have a legal address before registration process started. The location of leased premises will determine 
+the local authorities where LLC will shall be registered, including tax inspection. The legal address should be proven by
+signed lease agreement. Before establishment of the company, this agreement can be signed by the authorized representative 
+of any company-founder or at least the address should be known to start registration process.
+
+***The above documents (clauses 1-3) should be legalized or apostilled (if the country is a party to a convention Hague 
+Convention Abolishing the Requirement for Legalisation for Foreign Public Documents, October 5, 1965). If the company-founder
+is CIS-based legal entity the documents should be only notarized.***
+
+If any of the documents referenced above consists of more than one page, then all pages of such document should be stitched
+together to ensure that no page can be removed and that not only the last page but the whole document is notarized and
+apostilled (or legalized). Here in Ukraine these documents will be translated into Ukraine with notarization of the 
+translation before starting of registration process.
+
+The templates of the above documents along with English version of charter of LLC will be provided by the request. 
